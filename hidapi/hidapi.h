80cfd5205ebaafc9422183ee0aaeb3f75aa3ff30
Oleg Ostap <oostap1@logitech.com>
2018-06-06 08:57:56 -0700

    hid_send_input_report API
    Similar as to hid_get_input_report new API to send input report:
    hid_send_input_report.

--- hidapi/hidapi.h
+++ hidapi/hidapi.h
@@ -285,6 +285,34 @@ extern "C" {
 				-1 on error.
 		*/
 		int HID_API_EXPORT HID_API_CALL hid_get_input_report(hid_device *dev, unsigned char *data, size_t length);
+		
+		/** @brief Send a Feature report to the device.
+
+			Input reports are sent over the Control endpoint.  
+			The first byte of @p data[] must
+			contain the Report ID. For devices which only support a
+			single report, this must be set to 0x0. The remaining bytes
+			contain the report data. Since the Report ID is mandatory,
+			calls to hid_send_input_report() will always contain one
+			more byte than the report contains. For example, if a hid
+			report is 16 bytes long, 17 bytes must be passed to
+			hid_send_input_report(): the Report ID (or 0x0, for
+			devices which do not use numbered reports), followed by the
+			report data (16 bytes). In this example, the length passed
+			in would be 17.
+
+			@ingroup API
+			@param device A device handle returned from hid_open().
+			@param data The data to send, including the report number as
+				the first byte.
+			@param length The length in bytes of the data to send, including
+				the report number.
+
+			@returns
+				This function returns the actual number of bytes written and
+				-1 on error.
+		*/
+		int HID_API_EXPORT HID_API_CALL hid_send_input_report(hid_device *device, const unsigned char *data, size_t length);		
 
 		/** @brief Send a Feature report to the device.
 
